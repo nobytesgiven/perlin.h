11e90a9776d2dcfcf6ed615104652bf7e3dfab07
nobytesgiven <80068279+nobytesgiven@users.noreply.github.com>
2022-10-20 21:30:39 +0300

    Update noise.h

--- noise.h
+++ noise.h
@@ -50,7 +50,7 @@ void pseudo_gradient(double** grad, int *coords, unsigned int n) { // random n d
 	int uniq = coords[0];
 	
 	for (int i=1; i<(int)n; i++) {
-		uniq += primes[i%3]*coords[i];
+		uniq += primes[i%10]*coords[i];
 	}
 	
 	double pseudo_noise_unused;
